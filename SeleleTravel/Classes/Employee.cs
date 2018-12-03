315a1a369b173c6e019a68126ba005c0c99164fe
SimpiweSobici <stsobici@gmail.com>
2018-12-03 16:49:45 +0200

    unzip

---
+++ SeleleTravel/Classes/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleleTravel.Classes
+{
+    class Employee : IStaffMember
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public void updatePassword(string newPassword)
+        {
+            password = newPassword;
+        }
+
+        public void updateUsername(string newUsername)
+        {
+            username = newUsername;
+        }
+        public Employee(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
+
---
+++ SeleleTravel/Interfaces/IStaffMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleleTravel
+{
+    interface IStaffMember
+    {
+        string username { get; set; }
+        string password { get; set; }
+
+        void updatePassword(string newPassword);
+        void updateUsername(string newUsername);
+    }
+}
