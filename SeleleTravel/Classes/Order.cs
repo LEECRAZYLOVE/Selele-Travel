5af018ebcf254ea60d38b3f91a7074601063ea64
Sibusiso Lizalise Ncanywa <sbuda.ncanywa@gmail.com>
2018-12-04 12:20:41 +0200

    commit this!

---
+++ SeleleTravel/Classes/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleleTravel.Classes
+{
+    struct Order
+    {
+        public Quote approvedQuote;
+        public string orderNumber;
+        public DateTime timeOrdered;
+        public Order(Quote _approvedQuote, string _orderNumber)
+        {
+            approvedQuote = _approvedQuote;
+            orderNumber = _orderNumber;
+            timeOrdered = DateTime.Now;
+        }
+    }
+}
