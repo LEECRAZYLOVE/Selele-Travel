14a6f05358c1acd0346a5592d01dadc8feb053b7
LEECRAZYLOVE <LEECRAZYLOVE@users.noreply.github.com>
2018-12-12 13:48:05 +0200

    This is not important

--- SeleleTravel/Consultant.xaml.cs
+++ SeleleTravel/Consultant.xaml.cs
@@ -221,16 +221,16 @@ namespace SeleleTravel
 
             // Todo sql insertion
 
-            try
-            {
-                context.events.Add(currentEvent);
-                context.SaveChanges();
-            }
-            catch (System.Data.Entity.Validation.DbEntityValidationException ex)
-            {
-                var errorMessage = ex.EntityValidationErrors.First().ValidationErrors.First().ErrorMessage;
-                var propertyName = ex.EntityValidationErrors.First().ValidationErrors.First().PropertyName;
-            }
+           // try
+           // {
+           //     context.events.Add(currentEvent);
+           //     context.SaveChanges();
+           // }
+           // catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+           // {
+           //     var errorMessage = ex.EntityValidationErrors.First().ValidationErrors.First().ErrorMessage;
+           //     var propertyName = ex.EntityValidationErrors.First().ValidationErrors.First().PropertyName;
+           // }
             //catch (Exception ex)
             //{
             //    //other error
