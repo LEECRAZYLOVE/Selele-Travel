c1f6807c9719f1833a3a1d0cecabead7564bb93c
LEECRAZYLOVE <LEECRAZYLOVE@users.noreply.github.com>
2018-12-13 11:58:29 +0200

    Merged Manager and Boss with the staff class

--- SeleleTravel/Classes/Boss.cs
+++ SeleleTravel/Classes/Boss.cs
@@ -12,13 +12,13 @@ namespace SeleleTravel
     { 
 
 
-        public override List<Employee> employees { get => Employee.hiredEmployees; }
+        public override List<staff> employees { get => staff.hiredEmployees; }
 
         /// <summary>
         /// Fires an employee. Status : incomplete
         /// </summary>
         /// <param name="employee"></param>
-        public void fire(Employee employee)
+        public void fire(staff employee)
         {
             if (employees.Contains(employee))
                 employees.Remove(employee);
@@ -26,7 +26,7 @@ namespace SeleleTravel
             //Remove employee from database
 
             //This is for displaying to the user
-            string message = employee.names + " has been successfully fired.";
+            string message = employee.stafffirstnames + " has been successfully fired.";
             MessageBox.Show(message, "Alert..", MessageBoxButton.OK, MessageBoxImage.Information);
         }
         /// <summary>
@@ -34,27 +34,27 @@ namespace SeleleTravel
         /// Status : incomplete
         /// </summary>
         /// <param name="employee"></param>
-        public void hire(Employee employee)
+        public void hire(staff employee)
         {
-            employee.timeHired = DateTime.Now;
-            Employee.hiredEmployees.Add(employee);
+            employee.dateofhire = DateTime.Now;
+            staff.hiredEmployees.Add(employee);
 
             //Append changes to database
 
             //This part here is for alerting the user
-            string message = employee.names + " has been successfully hired.";
+            string message = employee.stafffirstnames + " has been successfully hired.";
             MessageBox.Show(message, "Alert..", MessageBoxButton.OK, MessageBoxImage.Information);
         }
         /// <summary>
         /// Makes a given employee a manager. Status : incomplete
         /// </summary>
         /// <param name="employee"></param>
-        public void promote(Employee employee)
+        public void promote(staff employee)
         {
             fire(employee);
-            Manager manager = new Manager(employee.names, employee.surname, employee.location);
-            manager.staff_id = employee.employeeID;
-            manager.dateofhire = employee.timeHired;
+            Manager manager = new Manager(employee.stafffirstnames, employee.stafflastname, employee.branch);
+            manager.staff_id = employee.staff_id;
+            manager.dateofhire = employee.dateofhire;
 
             //Reflect changes on database
 
